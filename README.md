7ff07c82cc259395e1ae1e5ae2dc71aea6935752
Chung Hak Ngor <chunghakngor@gmail.com>
2021-02-01 15:18:54 +1300

    updates to index.js with pub/sub model and add exp

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Testing Redis 
+
+Spin up redis on docker
+- `docker run --name redis-testing -p 6379:6379 redis`
+
+Run test script
+- `node index.js`
--- index.js
+++ index.js
@@ -12,3 +12,34 @@ client.set("username", "chung", redis.print);
 
 // getting the value of the username
 client.get("username", redis.print);
+
+// set userID to a random ID and expire aafter 2 seconds
+client.set("userID", "randomuserID");
+client.expire("userID", 2);
+
+// publish/subscribe model
+const publisher = redis.createClient();
+let total_users = 0;
+
+client.on("subscribe", (channel, count) => {
+  publisher.publish("current_users", "chung");
+  publisher.publish("current_users", "john");
+  publisher.publish("current_users", "bob");
+});
+
+client.on("message", (channel, msg) => {
+  total_users++;
+  // will console.log the channel name and message recieved
+  // can manually publish message using another client too
+  // eg. docker exec -it redis-testing redis-cli
+  //     publish current_users <user>
+  console.log(`${channel}:: ${msg} has been added!`);
+  if (total_users >= 3) {
+    console.log("Total current users has exceded 3!");
+    client.unsubscribe();
+    client.quit();
+    publisher.quit();
+  }
+});
+
+client.subscribe("current_users");
